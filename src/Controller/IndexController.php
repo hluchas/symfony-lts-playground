3434ad5b5083056c3938db8f8d4a2c697f9f2f43
Lukas Drahy <lukas@drahy.net>
2023-05-25 12:49:18 +0200

    Use annotations for routes

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController
 {
+    /**
+     * @Route ("/", name="app_inde_index")
+     */
     public function index(): Response
     {
         return new Response(
