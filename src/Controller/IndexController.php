6b3511d5b391d9d5418d64ddacebcb326e7c1d04
Lukas Drahy <lukas@drahy.net>
2023-05-25 12:41:31 +0200

    Add IndexController

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController
+{
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Hello world</body></html>'
+        );
+    }
+}
