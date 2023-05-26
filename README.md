1d4e9993c649ce33ecbf4d6854304c0371bcd27a
Lukas Drahy <lukas@drahy.net>
2023-05-26 13:41:43 +0200

    Add phpunit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+### Req
+ - PHP 8.2
+ - MySQL 8
+ - Node v20.2.0
 ### Installation (dev)
 ```bash
 composer install
@@ -5,4 +9,9 @@ bin/console doctrine:database:create
 bin/console doctrine:schema:create
 yarn install
 yarn watch
+```
+### Testing
+```bash
+composer all #run all checks
+composer test #run PHPUnit tests
 ```
---
+++ tests/Entity/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testName()
+    {
+        $name = 'name';
+
+        $product = new Product();
+        $product->setName($name);
+        self::assertSame($name, $product->getName());
+    }
+}
